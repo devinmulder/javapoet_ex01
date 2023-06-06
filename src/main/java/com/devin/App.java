3d5dc6b14a59c312a3115abc55d3f21cf0bc3bb3
amd <amd@devin.com>
2023-06-06 11:55:42 +0900

    commit.

--- src/main/java/com/devin/App.java
+++ src/main/java/com/devin/App.java
@@ -1,5 +1,11 @@
 package com.devin;
 
+import com.squareup.javapoet.JavaFile;
+import com.squareup.javapoet.MethodSpec;
+import com.squareup.javapoet.TypeSpec;
+
+import java.io.IOException;
+
 /**
  * Hello world!
  *
@@ -9,5 +15,24 @@ public class App
     public static void main( String[] args )
     {
         System.out.println( "Hello World!" );
+
+
+        MethodSpec methodSpec = MethodSpec.methodBuilder("getSuccess")
+                .build();
+
+
+        TypeSpec typeSpec = TypeSpec.classBuilder("Mudler")
+                .addMethod(methodSpec)
+                .build();
+
+
+        try {
+            JavaFile.builder("com.devin.generated", typeSpec)
+                    .build()
+                    .writeTo(System.out);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+
     }
 }
